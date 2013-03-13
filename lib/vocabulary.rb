98822def6b86bb6adb469fca67e59cd30de7ab24
Sergey Smagin <smaginsergey1310@gmail.com>
2013-03-13 17:03:03 +0400

    Replace CLI to it's proper place

---
+++ lib/vocabulary.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+require 'sequel'
+require 'optparse'
+
+module Vocabulary
+  # File, where vocabulary is.
+  #
+  DBFILE = File.expand_path('../../bin/vocabulary.db', __FILE__)
+
+  # Adapter to vocabulary.
+  #
+  DB = Sequel.sqlite(DBFILE)
+end
+
+require_relative 'voc/database'
